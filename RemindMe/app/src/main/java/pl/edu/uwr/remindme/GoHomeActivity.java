83c91b4088b3a63d2d0c43b26e4b251b18336601
kacpermisiek <315560@uwr.edu.pl>
2022-06-19 22:30:02 +0200

    Add google maps navigate to location intent

--- RemindMe/app/src/main/java/pl/edu/uwr/remindme/GoHomeActivity.java
+++ RemindMe/app/src/main/java/pl/edu/uwr/remindme/GoHomeActivity.java
@@ -2,6 +2,8 @@ package pl.edu.uwr.remindme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class GoHomeActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class GoHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_home);
+
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=Starbucks&mode=w");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
