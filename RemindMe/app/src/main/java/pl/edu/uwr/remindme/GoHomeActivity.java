2bdf1303b839b92287a48ce7992b9120ee5f96c2
kacpermisiek <315560@uwr.edu.pl>
2022-06-19 23:22:44 +0200

    Use shared preferences to set track destination

--- RemindMe/app/src/main/java/pl/edu/uwr/remindme/GoHomeActivity.java
+++ RemindMe/app/src/main/java/pl/edu/uwr/remindme/GoHomeActivity.java
@@ -3,6 +3,7 @@ package pl.edu.uwr.remindme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,7 +14,14 @@ public class GoHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_home);
 
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=Starbucks&mode=w");
+        SharedPreferences sharedPreferences = getSharedPreferences("MY_DATA", MODE_PRIVATE);
+
+        String googleString = "google.navigation:q=" +
+                sharedPreferences.
+                        getString("ADDRESS", "Wyspa Slodowa")
+                + "&mode=w";
+
+        Uri gmmIntentUri = Uri.parse(googleString);
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         startActivity(mapIntent);
